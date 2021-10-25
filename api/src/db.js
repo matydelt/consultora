6fa9343e28a96173c182d7d6c26319e65b754852
matydelt <matymanda@hotmail.com>
2021-10-25 15:55:58 -0300

    limpiando codigo

--- api/src/db.js
+++ api/src/db.js
@@ -60,12 +60,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Country, TouristActivity, Activities } = sequelize.models;
+const { } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-TouristActivity.belongsToMany(Country, { through: Activities });
-Country.belongsToMany(TouristActivity, { through: Activities });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
