2505a915c67ba7ae8d50c0511487c14411ea9d79
ZuperPatito <chony1208@gmail.com>
2021-10-27 11:08:05 -0300

    form cita
    form cita

--- client/src/FormCita/FormCita.jsx
+++ client/src/FormCita/FormCita.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
+export function validate(input) {
+  let errors = {};
+  if (!input.nombre) {
+    errors.nombre = "nombre is required";
+  } else if (!/\S+\S+/.test(input.nombre)) {
+    errors.nombre = "nombre is invalid";
+  }
+  return errors;
+}
 
 export default function FormCita() {
+  const [input, setInput] = useState({});
+  const [error, setError] = useState({});
+
+  const handleChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.nombre]: e.target.value,
+    });
+    setError(
+      validate({
+        ...input,
+        [e.target.nombre]: e.target.value,
+      })
+    );
+  };
+
   return (
-    <div className="formulario-cita">
+    <div classnombre="formulario-cita">
       <form>
         <label>Nombre</label>
-        <input type="text" />
+        <input nombre="nombre" type="text" required />
+
         <label>Apellido</label>
-        <input type="text" />
+        <input nombre="apellido" type="text" required />
+
         <label>DNI</label>
-        <input type="text" />
+        <input nombre="dni" type="text" required />
+
         <label>Teléfono</label>
-        <input type="tel" />
+        <input nombre="tel" type="tel" required />
+
+        <label>Email</label>
+        <input type="email" nombre="email" required />
+
+        <label>Mensaje</label>
+        <textarea nombre="mensaje" cols="30" rows="10" required></textarea>
       </form>
     </div>
   );
