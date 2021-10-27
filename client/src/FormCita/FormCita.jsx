e5600160032b7327f98f1551091c3e4f5e114734
ZuperPatito <chony1208@gmail.com>
2021-10-27 12:06:35 -0300

    function validate

--- client/src/FormCita/FormCita.jsx
+++ client/src/FormCita/FormCita.jsx
@@ -3,26 +3,51 @@ import React, { useState } from "react";
 export function validate(input) {
   let errors = {};
   if (!input.nombre) {
-    errors.nombre = "nombre is required";
+    errors.nombre = "nombre es requerido";
   } else if (!/\S+\S+/.test(input.nombre)) {
     errors.nombre = "nombre is invalid";
   }
+
+  if (!input.apellido) {
+    errors.apellido = "apellido es requerido";
+  } else if (!/\S+\S+/.test(input.nombre)) {
+    errors.apellido = "apellido is invalid";
+  }
+
+  if (!input.dni) {
+    errors.dni = "DNI es requerido";
+  } else if (/^\d{8}(?:[-\s]\d{4})?$/.test(input.dni)) {
+    errors.dni = "dni erroneo, ingrese el número sin puntos";
+  }
+
+  if (!input.telefono) {
+    errors.telefono = "Teléfono es requerido";
+  } else if (
+    /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/.test(
+      input.telefono
+    )
+  ) {
+    errors.telefono = "el Teléfono ingresado contiene errores";
+  }
+
   return errors;
 }
 
 export default function FormCita() {
+  const dispatch = useDispatch();
+
   const [input, setInput] = useState({});
   const [error, setError] = useState({});
 
   const handleChange = function (e) {
     setInput({
       ...input,
-      [e.target.nombre]: e.target.value,
+      [e.target.name]: e.target.value,
     });
     setError(
       validate({
         ...input,
-        [e.target.nombre]: e.target.value,
+        [e.target.name]: e.target.value,
       })
     );
   };
@@ -31,22 +56,28 @@ export default function FormCita() {
     <div classnombre="formulario-cita">
       <form>
         <label>Nombre</label>
-        <input nombre="nombre" type="text" required />
+        <input name="nombre" type="text" required onChange={handleChange} />
 
         <label>Apellido</label>
-        <input nombre="apellido" type="text" required />
+        <input name="apellido" type="text" required onChange={handleChange} />
 
         <label>DNI</label>
-        <input nombre="dni" type="text" required />
+        <input name="dni" type="number" required onChange={handleChange} />
 
         <label>Teléfono</label>
-        <input nombre="tel" type="tel" required />
+        <input name="tel" type="tel" required onChange={handleChange} />
 
         <label>Email</label>
-        <input type="email" nombre="email" required />
+        <input type="email" name="email" required onChange={handleChange} />
 
         <label>Mensaje</label>
-        <textarea nombre="mensaje" cols="30" rows="10" required></textarea>
+        <textarea
+          name="mensaje"
+          cols="30"
+          rows="10"
+          required
+          onChange={handleChange}
+        ></textarea>
       </form>
     </div>
   );
