5b72234fb73f6e8f20a1a6692ce2aabb53391657
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-10-28 18:29:40 -0300

    rutas en app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css';
-import HomePage from './components/home-page/HomePage';
-import { Route } from 'react-router-dom';
+import "./App.css";
+import HomePage from "./components/home-page/HomePage";
+import { Route } from "react-router-dom";
+import FormCita from "./components/FormCita/FormCita";
+import Perfiles from "./components/perfiles/Perfiles";
+import PerfilAbogado from "./components/perfilAbogado/PerfilAbogado";
 
 function App() {
   return (
     <div className="App container-fluid p-0">
-      <Route path="/">
+      <Route exact path="/">
         <HomePage />
+      </Route>
+      <Route path="/consulta">
+        <FormCita />
+      </Route>
+      <Route path="/abogados/:eMail">
+        <PerfilAbogado />
+      </Route>
+      <Route>
+        <Perfiles path="/abogados" />
       </Route>
     </div>
   );
