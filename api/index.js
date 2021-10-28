fd611b297fcb63668009e35d18dd53f3bca804cb
keilo-bravo <kesbto@gmail.com>
2021-10-28 18:25:02 -0300

    antes del merge

--- api/index.js
+++ api/index.js
@@ -22,17 +22,11 @@ const { conn } = require('./src/db.js');
 const { getMaterias } = require('./src/controllers/gets.js')
 
 // Syncing all the models at once.
-<<<<<<< HEAD
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT || "3000", async () => {
+  server.listen(process.env.PORT || "3001", async () => {
     const load = await getMaterias();
     console.log(`%s ${load}`);
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
-=======
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || "3001", () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
->>>>>>> 71a719d23f02591ee1d4f398ce66aa4a6380e210
   });
 });
 
