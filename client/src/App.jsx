1fbb6883c0b02bac92be6d6645bc6a5ef0db907e
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-10-29 22:08:18 -0300

    Boton de materia animado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Route path="/consulta">
         <FormCita />
       </Route>
-      <Route path="/abogados/:eMail">
+      <Route path="/perfil/:eMail">
         <PerfilAbogado />
       </Route>
       <Route>
