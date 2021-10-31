cf1a46602fbf7639265bb3fa928ea09bbac1d3c1
matydelt <matymanda@hotmail.com>
2021-10-31 20:02:20 -0300

    merge terminado

---
+++ client/src/components/Sign/alert.js
@@ -0,0 +1,56 @@
+import swal from 'sweetalert';
+
+export const createOK = () => {
+    swal({
+        title: "Usuario Creado Exitosamente",
+        text: '',
+        icon: "success",
+        button: "Aceptar"
+    })
+
+}
+
+export const correoNoOK = () => {
+    swal({
+        title: "Correo ya se encuentra registrado",
+        text: '',
+        icon: "warning",
+        button: "Aceptar"
+    })
+}
+
+export const dniNoOK = () => {
+    swal({
+        title: "DNI ya se encuentra registrado",
+        text: '',
+        icon: "warning",
+        button: "Aceptar"
+    })
+}
+
+export const sessionIN = () => {
+    swal({
+        title: "WELCOME",
+        text: '',
+        icon: "success",
+        button: "Aceptar"
+    })
+}
+
+export const sessionOUT = () => {
+    swal({
+        title: "GOODBYE",
+        text: '',
+        icon: "warning",
+        button: "Aceptar"
+    })
+}
+
+export const sessionERR = () => {
+    swal({
+        title: "Usuario o Contraseña incorrecta",
+        text: '',
+        icon: "error",
+        button: "Aceptar"
+    })
+}
