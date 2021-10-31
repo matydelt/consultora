3bb76fdac45e563bd76cefe01852ccf31fae0721
matydelt <matymanda@hotmail.com>
2021-10-31 19:03:36 -0300

    adm page agregada

--- client/src/components/adminPage/adminPage.jsx
+++ client/src/components/adminPage/adminPage.jsx
@@ -1,17 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { getUsuarios, setAbogado } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
 
 export default function AdminPage() {
-    const [alert, setAlert] = useState(true);
     const dispatch = useDispatch()
     const adm = useSelector(state => state.adm)
     const allUsers = useSelector(state => state.usuarios)
     let arrBolean = []
     var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-    var alertTrigger = document.getElementById('liveAlertBtn')
     useEffect(() => {
         dispatch(getUsuarios())
     }, [])
@@ -43,55 +41,55 @@ export default function AdminPage() {
 
 
     return (
-        // !adm ? <Redirect to="/" /> :
-        <div className="ms-5 me-5 mt-3 mb-3">
+        !adm ? <Redirect to="/" /> :
+            <div className="ms-5 me-5 mt-3 mb-3">
 
-            <table className="table table-striped  ">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Abogado</th>
-                        <th scope="col">Usuario</th>
-                        <th scope="col">Dni</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {allUsers?.map((e, i) => {
-                        if (e.abogadoId) {
-                            arrBolean.push(false)  // bandera avisa a la api de que ya tiene stado de abogado y debe eliminarlo
-                            return (
-                                <tr key={i}>
-                                    <th scope="row">{i}</th>
-                                    <td >
-                                        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value={e.eMail} onInput={(e) => hadleChange(e, i)} defaultChecked />
-                                    </td>
-                                    <td>{e.eMail}</td>
-                                    <td>{e.personaDni} </td>
-                                </tr>
-                            )
-                        }
-                        else {
-                            arrBolean.push(true)
-                            return (
-                                <tr key={i}>
-                                    <th scope="row">{i}</th>
-                                    <td className="">
-                                        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value={e.eMail} onInput={(e) => hadleChange(e, i)} />
-                                    </td>
-                                    <td>{e.eMail} </td>
-                                    <td>{e.personaDni} </td>
+                <table className="table table-striped  ">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Abogado</th>
+                            <th scope="col">Usuario</th>
+                            <th scope="col">Dni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allUsers?.map((e, i) => {
+                            if (e.abogadoId) {
+                                arrBolean.push(false)  // bandera avisa a la api de que ya tiene stado de abogado y debe eliminarlo
+                                return (
+                                    <tr key={i}>
+                                        <th scope="row">{i}</th>
+                                        <td >
+                                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value={e.eMail} onInput={(e) => hadleChange(e, i)} defaultChecked />
+                                        </td>
+                                        <td>{e.eMail}</td>
+                                        <td>{e.personaDni} </td>
+                                    </tr>
+                                )
+                            }
+                            else {
+                                arrBolean.push(true)
+                                return (
+                                    <tr key={i}>
+                                        <th scope="row">{i}</th>
+                                        <td className="">
+                                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value={e.eMail} onInput={(e) => hadleChange(e, i)} />
+                                        </td>
+                                        <td>{e.eMail} </td>
+                                        <td>{e.personaDni} </td>
 
-                                </tr>
-                            )
-                        }
-                    })}
+                                    </tr>
+                                )
+                            }
+                        })}
 
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
 
-            <div id="liveAlertPlaceholder"></div>
-        </div>
+                <div id="liveAlertPlaceholder"></div>
+            </div>
 
     )
 
