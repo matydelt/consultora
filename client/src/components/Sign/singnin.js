1eaf7919a2f3e692d18a43ed796e89cfcf89ba7d
keilo-bravo <kesbto@gmail.com>
2021-11-01 18:34:34 -0300

    login local

--- client/src/components/Sign/singnin.js
+++ client/src/components/Sign/singnin.js
@@ -4,6 +4,7 @@ import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvide
 import Logo  from '../home-page/assets/img/buffet-buffet-law.png'
 import { getUsuario, postUsuario, getPersonas, getUsuarios } from "../../redux/actions";
 import { sessionERR, sessionIN, sessionOUT, createOK, correoNoOK, dniNoOK } from "./alert";
+import { Link } from "react-router-dom";
 import md5 from 'md5'
 
 
@@ -196,6 +197,16 @@ export const Signin = () =>{
                                             <button className="btn btn-primary btn-block" onClick={loginGoogle}>
                                                 Google
                                             </button>
+                                        </div>
+                                        <div className="form-group">
+                                            <h6>-- -- -- -- -- -- -- -- -- -- -- -- </h6>
+                                        </div>
+                                        <div className="form-group">
+                                            <Link to="/signup">
+                                                <label>
+                                                ← ← ← Go to Register 
+                                                </label>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
