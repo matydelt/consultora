f2cbbe8a63e8d1045d1683d48b5559422dc6a6e1
ezequielcoronel <coronelezequiel94@gmail.com>
2021-11-04 15:49:43 -0300

    Update homeUsuario.jsx

--- client/src/components/home-Usuario/homeUsuario.jsx
+++ client/src/components/home-Usuario/homeUsuario.jsx
@@ -4,7 +4,10 @@ import React from "react";
 export default function homeUsuario() {
     return (
         <div>
-            
+            <navUsuario>
+            <form>
+
+            </form>
         </div>
     )
 }
