152a9d8c5d5ee13c83903fbc4f9920e800d0bdd1
ajlveloper <ajveloper@gmail.com>
2021-11-18 22:38:03 -0430

    Signin, Signup, RegistroGoogle, NewPass Listos con Estilos

--- client/src/components/Sign/singnin.js
+++ client/src/components/Sign/singnin.js
@@ -83,38 +83,38 @@ export const Signin = () => {
       });
   };
 
-  const GoTo = async () => {
-    if (
-      usuarios.some((e) => e.eMail.toString() === eMail.toString()) ||
-      personas.some((e) => e.dni.toString() === dni.toString())
-    ) {
-      usuarios.some((e) => e.eMail.toString() === eMail.toString())
-        ? correoNoOK()
-        : dniNoOK();
-    } else {
-      dispatch(
-        postUsuario({
-          eMail: eMail,
-          firstName: firstName,
-          dni: dni,
-          lastName: lastName,
-          celular: celular,
-          password: md5(password),
-        })
-      )
-        .then(() => {
-          dispatch(getUsuario({ eMail: eMail }));
-        })
-        .catch((error) => {});
-      createOK();
-      setFirstName("");
-      setLastName("");
-      setPhone("");
-      setDni("");
-      setEmail("");
-      setPassword("");
-    }
-  };
+  // const GoTo = async () => {
+  //   if (
+  //     usuarios.some((e) => e.eMail.toString() === eMail.toString()) ||
+  //     personas.some((e) => e.dni.toString() === dni.toString())
+  //   ) {
+  //     usuarios.some((e) => e.eMail.toString() === eMail.toString())
+  //       ? correoNoOK()
+  //       : dniNoOK();
+  //   } else {
+  //     dispatch(
+  //       postUsuario({
+  //         eMail: eMail,
+  //         firstName: firstName,
+  //         dni: dni,
+  //         lastName: lastName,
+  //         celular: celular,
+  //         password: md5(password),
+  //       })
+  //     )
+  //       .then(() => {
+  //         dispatch(getUsuario({ eMail: eMail }));
+  //       })
+  //       .catch((error) => {});
+  //     createOK();
+  //     setFirstName("");
+  //     setLastName("");
+  //     setPhone("");
+  //     setDni("");
+  //     setEmail("");
+  //     setPassword("");
+  //   }
+  // };
 
   const logout = async () => {
     await signOut(auth)
@@ -180,154 +180,8 @@ export const Signin = () => {
         <NavBarGeneral />
         {!!usuario.firstName ? (
           <Redirect to="/user/panel" />
-        ) : // <div className="container p-4">
-        //   <div className="row">
-        //     <div className="col-md-4 mx-auto">
-        //       <div className="card text-center">
-        //         <div className="card-header">
-        //           <h2>Bienvenido</h2>
-        //         </div>
-        //         <div className="card-header">
-        //           <h3>
-        //             {displayname
-        //               ? displayname
-        //               : `${usuario.firstName} ${usuario.lastName}`}
-        //           </h3>
-        //         </div>
-        //         <img
-        //           src={Logo}
-        //           alt="Logo Consultora"
-        //           className="card-img-top mx-auto m-2 rounded-circle w-50"
-        //         />
-        //         <div className="card-body">
-        //           <button
-        //             className="btn btn-primary btn-block"
-        //             onClick={logout}
-        //           >
-        //             Cerrar sesión
-        //           </button>
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
-        displayname ? (
-          <div className="body_displayname">
-            <div className="bg_blue_image_displayname">
-              <div className="overlay_displayname overlay_right_displayname">
-                <div>
-                  <img src={LogoBlanco} alt="Logo" />
-                  <h4>
-                    ¿Tienes una cuenta? <br /> Presione "Iniciar Sesión".
-                  </h4>
-                  <ButtonsNav text="Iniciar Sesion" link="/ingreso" />
-                </div>
-              </div>
-              <div className="form_container registerEmail_in_container">
-                <div className="registerEmail">
-                  <h3>Registro</h3>
-                  <div className="singn_input_login">
-                    <input
-                      type="type"
-                      value={firstName}
-                      name="firstName"
-                      autoComplete="off"
-                      placeholder=" Nombre"
-                      className=""
-                      autoFocus
-                      required
-                      onChange={(e) => {
-                        setFirstName(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <div className="singn_input_login">
-                    <input
-                      type="type"
-                      value={lastName}
-                      name="lastName"
-                      autoComplete="off"
-                      placeholder=" Apellido"
-                      className=""
-                      autoFocus
-                      required
-                      onChange={(e) => {
-                        setLastName(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <div className="singn_input_login">
-                    <input
-                      type="text"
-                      value={dni}
-                      name="DNI"
-                      autoComplete="off"
-                      placeholder="DNI : 1234567"
-                      className=""
-                      required
-                      onChange={(e) => {
-                        setDni(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <div className="singn_input_login">
-                    <input
-                      type="text"
-                      value={celular}
-                      name="Number"
-                      autoComplete="off"
-                      placeholder="Teléfono : 11 1111-1111"
-                      className=""
-                      required
-                      onChange={(e) => {
-                        setPhone(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <div className="singn_input_login">
-                    <input
-                      type="text"
-                      value={eMail}
-                      name="Mail"
-                      disabled="on"
-                      autoComplete="off"
-                      placeholder="Email: ejemplo@ejemplo.com"
-                      className=""
-                      required
-                    />
-                  </div>
-                  <div className="singn_input_login">
-                    <input
-                      type="password"
-                      value={password}
-                      disabled="on"
-                      name="password"
-                      autoComplete="off"
-                      placeholder="Contraseña"
-                      className=""
-                      required
-                    />
-                  </div>
-                  <div className="divButton_register_email">
-                    <button
-                      className=""
-                      onClick={GoTo}
-                      disabled={
-                        firstName === "" ||
-                        lastName === "" ||
-                        dni === "" ||
-                        celular === "" ||
-                        eMail === "" ||
-                        password.length < 6
-                      }
-                    >
-                      Register
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        ) : displayname ? (
+          <Redirect to="/registroGoogle" />
         ) : (
           <div className="body_login">
             <div id="container" className="bg_blue_image pt-4">
@@ -413,7 +267,7 @@ export const Signin = () => {
                   </div>
                   <div className="forgot_login">
                     <p>¿Olvidaste tu contraseña?</p>
-                    <Link to="/Cambiopass">Presiona aquí</Link>
+                    <ButtonsNav text="Presiona aquí" link="/Cambiopass" />
                   </div>
                 </div>
               </div>
