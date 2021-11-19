fa296c8d6df4bd79ef098c4ff9c401caba3d6b7a
matydelt <matymanda@hotmail.com>
2021-11-19 16:14:30 -0300

    intentando deployar

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 conn.sync({ force: false }).then(() => {
-  server_port.listen(server_port, server_host, function () {
+  server.listen(server_port, server_host, function () {
     console.log('Listening on port %d', server_port);
   });
 });
